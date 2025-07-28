2c1057e315a51f4347921fc46d54f47460790004
pavithrakumar22 <pavithrakumarsahu60@gmail.com>
2025-07-28 11:37:36 +0530

    added index.js\

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello imas backend");
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('IMAS Backend is running ✅');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
