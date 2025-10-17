513dca749b40b007a5711fe32e986ea6a8fbcf13
vivek <vivek.pandit3190@gmail.com>
2025-10-17 15:24:56 +0530

    simplify

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ import cors from 'cors';
 import combinedRoutes from './api/routes/combined.js';
 import mongoose from 'mongoose';
 import authRoutes from './api/routes/authRoutes.js';
-<<<<<<< HEAD
 import interviewRoutes from "./api/routes/interview.js"
 // import handleUser from "./api/routes/handleUser.js"
-=======
-import langgraphroutes from './api/routes/langgraphroutes.js';
->>>>>>> 2a08880c8db3c8811019a3f6d8bd02da89710fc7
 import { spawn } from 'child_process';
 
 dotenv.config();
@@ -33,12 +29,8 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use('/api/auth', authRoutes);
 app.use('/api/combined', combinedRoutes);
-<<<<<<< HEAD
 app.use('/api/interview', interviewRoutes);
 // app.use("/api/handle-user", handleUser);
-=======
-app.use('/api/lang', langgraphroutes);
->>>>>>> 2a08880c8db3c8811019a3f6d8bd02da89710fc7
 
 app.get('/health', (req, res) => res.json({ status: "Node is alive" }));
 
